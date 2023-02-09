7be81c006cc8dd5833b8772bfb5fc3b3eb845aaf
Michael Carroll <michael@openrobotics.org>
2023-02-09 14:27:08 -0600

    Add caching and log artifacts to GitHub actions
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- .github/actions/bazel-ci-jammy/run.sh
+++ .github/actions/bazel-ci-jammy/run.sh
@@ -4,61 +4,66 @@ set -x
 set -e
 
 BAZEL_ARGS=$1
+WORKSPACE=~/gz
+BAZEL_CACHE=$GITHUB_WORKSPACE/bazel_cache
+BAZEL=$BAZEL_CACHE/bazelisk-linux-amd64
 
-echo ::group::Install tools: apt
-apt update 2>&1
-apt -y install \
-  build-essential \
-  cppcheck \
-  curl \
-  git \
-  gnupg \
-  lsb-release \
-  python3-pip \
-  wget
-
-cd "$GITHUB_WORKSPACE"
 SYSTEM_VERSION=`lsb_release -cs`
 SOURCE_DEPENDENCIES="`pwd`/.github/ci/dependencies.yaml"
 SOURCE_DEPENDENCIES_VERSIONED="`pwd`/.github/ci-$SYSTEM_VERSION/dependencies.yaml"
-echo ::endgroup::
 
-echo ::group::Install tools: pip
-pip3 install -U pip vcstool colcon-common-extensions
-echo ::endgroup::
+cd "$GITHUB_WORKSPACE"
 
-# Install bazelisk
-wget https://github.com/bazelbuild/bazelisk/releases/download/v1.16.0/bazelisk-linux-amd64
-mv ./bazelisk-linux-amd64 /usr/bin/bazel
-chmod +x /usr/bin/bazel
+# Restore cache
+if [ ! -d "$BAZEL_CACHE" ]; then
+  mkdir -p ${BAZEL_CACHE}
+fi
+rm -rf ~/.cache
+ln -sf ${BAZEL_CACHE} ~/.cache
+
+# Install baselisk if it is not already installed
+if [ ! -f ${BAZEL} ]; then
+  wget https://github.com/bazelbuild/bazelisk/releases/download/v1.16.0/bazelisk-linux-amd64 -O ${BAZEL}
+fi
+chmod +x ${BAZEL}
 
 # Import repos
-mkdir -p /gz
-cd /gz
-cp -R /github/workspace /gz/bazel
-vcs import . < /github/workspace/example/bazel.repos
+mkdir -p ${WORKSPACE}/bazel
+cd ${WORKSPACE}
+shopt -s extglob
+# Copy relevant bazel files into the build space
+cp -R ${GITHUB_WORKSPACE}/!(gz|.git|.github|bazel_cache) ${WORKSPACE}/bazel
+vcs import . < ${WORKSPACE}/bazel/example/bazel.repos
 
 echo ::group::Install dependencies from binaries
 EXCLUDE_APT="libignition|libgz|libsdformat|libogre|dart"
 UBUNTU_VERSION=`lsb_release -cs`
 ALL_PACKAGES=$( \
   sort -u $(find . -iname 'packages-'$UBUNTU_VERSION'.apt' -o -iname 'packages.apt') | grep -Ev $EXCLUDE_APT | tr '\n' ' ')
-apt-get install --no-install-recommends --quiet --yes $ALL_PACKAGES
+sudo apt install --no-install-recommends --quiet --yes $ALL_PACKAGES
 echo ::endgroup::
 
-ln -sf /gz/bazel/example/WORKSPACE.example /gz/WORKSPACE
-ln -sf /gz/bazel/example/BUILD.bazel.example /gz/BUILD.bazel
-ln -sf /gz/bazel/example/bazelrc.example /gz/.bazelrc
-ln -sf /gz/bazel/example/bazelproject.example /gz/.bazelproject
+ln -sf ${WORKSPACE}/bazel/example/WORKSPACE.example ${WORKSPACE}/WORKSPACE
+ln -sf ${WORKSPACE}/bazel/example/BUILD.bazel.example ${WORKSPACE}/BUILD.bazel
+ln -sf ${WORKSPACE}/bazel/example/bazelrc.example ${WORKSPACE}/.bazelrc
+ln -sf ${WORKSPACE}/bazel/example/bazelproject.example ${WORKSPACE}/.bazelproject
 
 echo ::group::Bazel query
-bazel query $BAZEL_ARGS
+${BAZEL} query $BAZEL_ARGS
 echo ::endgroup::
 
 echo ::group::Bazel build
-bazel build $BAZEL_ARGS
+${BAZEL} build $BAZEL_ARGS
 echo ::endgroup::
 
 echo ::group::Bazel test
-bazel test $BAZEL_ARGS
+${BAZEL} test $BAZEL_ARGS
 echo ::endgroup::
+
+echo ::group::Bazel test
+ls -la ${WORKSPACE}
+ls -la ${WORKSPACE}/bazel-testlogs
+ls -la ${BAZEL_CACHE}
+echo ::endgroup::
+
+cp -RL ${WORKSPACE}/bazel-testlogs ${GITHUB_WORKSPACE}/bazel-testlogs
